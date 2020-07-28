9c6045910024a5ac5b0a127d85c5026390645155
albert <albertkingdom@gmail.com>
2020-07-28 13:04:11 +0800

    Updates

---
+++ precache-manifest.412face4e2cf3482a61ac72c0c9caeaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a9355453bb6635857746625de54245",
+    "url": "/covid19info/index.html"
+  },
+  {
+    "revision": "4dce0949d900d6c41d45",
+    "url": "/covid19info/static/css/main.e469a2da.chunk.css"
+  },
+  {
+    "revision": "42cc0c4b6809eb512d21",
+    "url": "/covid19info/static/js/2.ad643efa.chunk.js"
+  },
+  {
+    "revision": "4dce0949d900d6c41d45",
+    "url": "/covid19info/static/js/main.6bb055c7.chunk.js"
+  },
+  {
+    "revision": "779b82217d5a2a0ef632",
+    "url": "/covid19info/static/js/runtime~main.65e5bd4d.js"
+  }
+]);
